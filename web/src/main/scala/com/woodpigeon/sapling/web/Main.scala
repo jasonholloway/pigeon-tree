bc118687864039e108d8e2f5745421cdffebaebe
Jason Holloway <miceeatnicerice@hotmail.com>
2017-12-01 23:39:12 +0000

    to and fro of sprout and extension

--- web/src/main/scala/com/woodpigeon/sapling/web/Main.scala
+++ web/src/main/scala/com/woodpigeon/sapling/web/Main.scala
@@ -3,54 +3,67 @@ package com.woodpigeon.sapling.web
 import org.scalajs.dom._
 import scala.scalajs.js.timers._
 
-case class Vector(x: Double, y: Double)
+case class Vector(x: Double = 0, y: Double = 0)
+
+case class GrowContext(sap: Double)
+
+case class ProjectContext(v0: Vector, aB: Double)
 
 
-case class GrowContext(baseX: Double, baseY: Double, baseA: Double)
-
-trait Growable {
-  def grow(ctx: GrowContext) : Option[Growable]
+trait Sprouting {
+  def project(ctx: ProjectContext): Option[Extended]
 }
 
-case class Branch(
-  angle: Double,
-  start: Vector,
-  end: Vector,
-  children: List[Growable] = Nil,
-  next: Option[Growable] = None,
-  g: Double = 0.1,
-  version: Int = 0) extends Growable {
+trait Extended {
+  def grow(ctx: GrowContext) : Option[Sprouting]
+}
 
-  private val d = 0.7
-  private val l = 10
 
-  def grow(ctx: GrowContext): Option[Growable] = ctx match {
-    case GrowContext(x0, y0, baseA) => {
-      val a = (baseA + angle) % Math.PI
+case class ExtendedBranch(branch: SproutingBranch, v0: Vector, v1: Vector, a: Double, children: List[Extended]) extends Extended {
 
-      val x1 = x0 + (Math.sin(a) * g)
-      val y1 = y0 + (Math.cos(a) * g)
+  private val growthRate = 0.8
+  private val stature = 10
 
-      Some(copy(
+  override def grow(ctx: GrowContext): Option[Sprouting] = branch match {
+    case SproutingBranch(_, _, _, g, version)  =>
+      Some(branch.copy(
         version = version + 1,
-        start = Vector(x0, y0),
-        end = Vector(x1, y1),
-        g = g + (d - (d * Math.cos(2 * Math.sqrt((10 / l) * g)))) / 2,
-        next = next match {
-          case Some(growable) => growable.grow(GrowContext(x1, y1, a))
-          case None if version > 10 => Some(Branch(-0.3 * a, Vector(x1, y1), Vector(x1, y1)))
-          case _ => None
-        },
+        g = g + (growthRate - (growthRate * Math.cos(2 * Math.sqrt((10 / stature) * g)))) / 2,
         children = {
-          val grownChildren = children flatMap { _.grow(GrowContext(x1, y1, a)) }
+          val grownChildren = children flatMap { _.grow(GrowContext(1)) }
 
-          if(version > 10 && children.isEmpty && Math.random() > 0.99) {
-            val childAngle = if(Math.random() > 0.5) Math.PI / 2 else -Math.PI / 2
-            Branch(childAngle, Vector(x1, y1), Vector(x1, y1)) :: grownChildren
-          } else grownChildren
+          if(children.isEmpty) {
+            if(g > 7 && Math.random() > 0.97) {
+              SproutingBranch(-0.3 * a) :: grownChildren
+            }
+            else grownChildren
+          }
+          else {
+            if(Math.random() > 0.99) {
+              val childAngle = if(Math.random() > 0.5) Math.PI / 2 else -Math.PI / 2
+              SproutingBranch(childAngle) :: grownChildren
+            }
+            else grownChildren
+          }
         }
       ))
-    }
+  }
+
+}
+
+
+case class SproutingBranch(aOrig: Double, children: List[Sprouting] = Nil, next: Option[Sprouting] = None, g: Double = 0.1, version: Int = 0) extends Sprouting {
+
+  override def project(ctx: ProjectContext): Option[Extended] = ctx match {
+    case ProjectContext(v0@Vector(x0, y0), aBase) =>
+      val a = (aBase + aOrig) % Math.PI
+
+      val v1 = Vector(
+        x0 + (Math.sin(a) * g),
+        y0 + (Math.cos(a) * g)
+      )
+
+      Some(ExtendedBranch(this, v0, v1, a, children.flatMap { _.project(ProjectContext(v1, a)) }))
   }
 
 }
@@ -66,20 +79,25 @@ case class RenderContext(offset: Vector, first: Boolean = false)
 
 object Main {
 
-  def render(ctx: RenderContext, el: Growable) : String = ctx match {
+  def render(ctx: RenderContext, el: Extended) : String = ctx match {
     case RenderContext(offset, isFirst) =>
       el match {
-        case Branch(_, start, end, children, next, _, _) => {
-          val inner = ((next ++ children) map {
+        case ExtendedBranch(_, v0, v1, _, children) => {
+          val inner = (children map {
             render(RenderContext(offset), _)
           }).mkString
 
-          (start, end) match {
-            case (Vector(x0, y0), Vector(x1,y1)) => {
+          (v0, v1) match {
+            case (Vector(x0, y0), Vector(x1, y1)) => {
+              val ox0 = offset.x + x0
+              val oy0 = offset.y - y0
+              val ox1 = offset.x + x1
+              val oy1 = offset.y - y1
+
               if (isFirst)
-                s"M${offset.x + x0} ${offset.y - y0} ${inner}L${offset.x + x0} ${offset.y - y0}"
+                s"M$ox0 $oy0 ${inner}L$ox0 $oy0"
               else
-                s"L${offset.x + x1} ${offset.y - y1} ${inner}L${offset.x + x0} ${offset.y - y0} "
+                s"L$ox1 $oy1 ${inner}L$ox0 $oy0 "
             }
           }
         }
@@ -92,24 +110,26 @@ object Main {
 
     val elPath = document.getElementById("treePath").asInstanceOf[svg.Path]
 
-    var tree: Option[Growable] = Some(Branch(0, Vector(0, 0), Vector(0, 0)))
+    var sprouting: Option[Sprouting] = Some(SproutingBranch(0))
+    var extended: Option[Extended] = None
+    var i = 0
 
     println("Starting!")
 
-    setInterval(100) {
-      tree foreach { g =>
-        tree = g.grow(GrowContext(0, 0, 0))
-                  .flatMap { _.grow(GrowContext(0, 0, 0)) }
-                  .flatMap { _.grow(GrowContext(0, 0, 0)) }
-                  .flatMap { _.grow(GrowContext(0, 0, 0)) }
 
-        val pathData = render(RenderContext(Vector(400, 500), first = true), g)
+    setInterval(20) {
+      extended = sprouting flatMap { _.project(ProjectContext(Vector(), 0)) }
 
-        println(pathData)
+      sprouting = extended flatMap { _.grow(GrowContext(1)) }
 
-        elPath.setAttribute("d", pathData)
+      if (i % 8 == 0) {
+        extended foreach { e =>
+          val pathData = render(RenderContext(Vector(400, 500), first = true), e)
+          elPath.setAttribute("d", pathData)
+        }
       }
 
+      i = i + 1
     }
 
   }
